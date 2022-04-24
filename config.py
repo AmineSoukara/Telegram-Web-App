c81e74d8fd19401ee56e63bdb8b37e30ea972b29
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2022-04-24 17:03:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 WEBHOOK_HOST = environ.get("WEBHOOK_HOST", "") # "your_domain.com"
 WEBHOOK_PATH = environ.get("WEBHOOK_PATH", "/demo") # "/web_app_example"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = 8000
+WEBAPP_HOST = environ.get("WEBAPP_HOST", "0.0.0.0")
+WEBAPP_PORT = int(environ.get("WEBAPP_PORT", 8000))
