26e0932ce815e3c80ed0efbc96c38c748d6c910d
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2022-04-24 16:50:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,65 @@
-from flask import Flask, request, abort, send_file, render_template
-from telebot import TeleBot, types
+import os
 
-from config import *
+from flask import Flask, jsonify, render_template, request
+from pyrogram import Client, filters
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 from utils import parse_init_data
 
-bot = TeleBot(BOT_TOKEN, parse_mode="HTML")
-app = Flask(__name__, static_url_path='/static')
+from config import *
+
+app = Flask(__name__, static_url_path="/static")
 
 
-# @app.post(WEBHOOK_PATH)
-@app.route('/' + BOT_TOKEN, methods=['POST'])
-def process_webhook_post():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
+API_ID = 2059351
+API_HASH = "055ad1774b838870be128567b7a4c04a"
+BOT_TOKEN = "5186450665:AAEJjATsJZJvQ0vuoGS0PDHUsdYjFhEv6Ug"
+
+bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+bot.start()
 
 
-@app.get('/')
+@app.get("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.get('/hello')
-def indevvvvx():
-    return "hello"
 
-@app.post('/submitOrder')
+@app.route("/getMe")
+def getMe():
+    print("start")
+    me = bot.get_me()
+    print("end")
+    print(me)
+    return jsonify(me)
+
+
+@app.post("/submitOrder")
 def submit_order():
     data = request.json
-    init_data = parse_init_data(token=BOT_TOKEN, raw_init_data=data['initData'])
+    init_data = parse_init_data(token=BOT_TOKEN, raw_init_data=data["initData"])
     if init_data is False:
         return False
 
-    query_id = init_data['query_id']
+    query_id = init_data["query_id"]
 
     result_text = "<b>Order summary:</b>\n\n"
-    for item in data['items']:
+    for item in data["items"]:
         name, price, amount = item.values()
         result_text += f"{name} x{amount} — <b>{price}</b>\n"
-    result_text += '\n' + data["totalPrice"]
+    result_text += "\n" + data["totalPrice"]
 
-    result = types.InlineQueryResultArticle(
+    result = InlineQueryResultArticle(
         id=query_id,
-        title='Order',
-        input_message_content=types.InputTextMessageContent(message_text=result_text, parse_mode='HTML'))
-    bot.answer_web_app_query(query_id, result)
-    return ''
-
-
-@bot.message_handler(commands=['start'])
-def cmd_start(message: types.Message):
-    markup = types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text="Order Food",
-                    web_app=types.WebAppInfo(url=f'https://{WEBHOOK_HOST}'),
-                )
-            ]
-        ]
+        title="Order",
+        input_message_content=InputTextMessageContent(result_text, parse_mode="HTML"),
     )
-    bot.send_message(message.from_user.id, "<b>Hey!</b>\nYou can order food here!", reply_markup=markup)
+    bot.answer_web_app_query(query_id, result)
+    return ""
 
 
-@bot.message_handler(func=lambda message: message.via_bot)
-def ordered(message: types.Message):
-    bot.reply_to(message, '<b>Thank you for your order!</b>\n(It will not be delivered)')
-
-
-def main():
-    bot.delete_webhook()
-    # bot.set_webhook(WEBHOOK_URL)
-    bot.set_webhook(url=WEBHOOK_HOST + BOT_TOKEN)
-    app.run(host=WEBAPP_HOST, port=WEBAPP_PORT)
+@bot.on_message(filters.private & filters.command(["start", "help"]))
+def cmd_start(c, m):
+    m.reply("done")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host=WEBAPP_HOST, port=WEBAPP_PORT)
